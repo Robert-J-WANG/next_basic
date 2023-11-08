59e8240f28da3b50cd39b4bea50c632425e65d44
Robert-J-WANG <robin.nz420@gmail.com>
2023-11-08 21:13:04 +1300

    07-页面中动态获取数据

--- app/product/page.tsx
+++ app/product/page.tsx
@@ -1,13 +1,29 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 
 export default function page() {
+  const [date, setDate] = useState<string>("");
+  const [products, setProducts] = useState<any[]>([]); // 指定初始值为一个空数组
+  const fetchData = async () => {
+    const res = await axios.get("https://dummyjson.com/products");
+    console.log(res);
+    setProducts(res.data.products);
+    setDate(new Date().toString());
+  };
+  useEffect(() => {
+    fetchData();
+  }, []);
   return (
     <div className="container">
       <h1>Product Page</h1>
-      <div className="box">
-        <h2>product 1</h2>
-        <h2>product 2</h2>
-        <h2>product 3</h2>
+      <h2>{date}</h2>
+      <div className="box flex flex-col gap-5">
+        {products.map((product) => (
+          <h1 key={product.id}>
+            {product.title} /{product.brand} : {product.price}
+          </h1>
+        ))}
       </div>
     </div>
   );
