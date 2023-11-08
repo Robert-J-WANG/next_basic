69f411950105858e2b32fc7534ac4381846225de
Robert-J-WANG <robin.nz420@gmail.com>
2023-11-08 19:34:59 +1300

    03-内嵌动态路由

---
+++ app/product/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,11 @@
+export default function page({
+  params,
+}: {
+  params: { productId: string; reviewId: string };
+}) {
+  return (
+    <h1>
+      Review {params.reviewId} for product {params.productId}
+    </h1>
+  );
+}
---
+++ app/product/[productId]/reviews/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+  return <h1>reviews about the product</h1>;
+}
