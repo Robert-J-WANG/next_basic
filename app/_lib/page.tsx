d691af2ed6ae312fecb8e653c2a37c230cdd3b23
Robert-J-WANG <robin.nz420@gmail.com>
2023-11-08 19:54:43 +1300

    05-文件共直和私有文件

---
+++ app/_lib/page.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function page() {
+  return <div>_开头的文件是私有文件，无法访问</div>;
+}
---
+++ app/dashboard/lineChart.tsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function lineChart() {
+  return <div>无法访问此文件，只能访问page.tsx文件</div>;
+}
