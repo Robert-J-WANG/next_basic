7bd406548fedbe70241f85c93409b725acfdf265
Robert-J-WANG <robin.nz420@gmail.com>
2023-11-09 21:03:27 +1300

    next Link

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
     <div className="container">
       <h1>Welcome Home</h1>
       <div className="box">this is a home page box</div>
+      <Link href="/about">
+        <button>about</button>
+      </Link>
+      <Link href="/blog">
+        <button>blog</button>
+      </Link>
+      <Link href="/product">
+        <button>product</button>
+      </Link>
     </div>
   );
 }
